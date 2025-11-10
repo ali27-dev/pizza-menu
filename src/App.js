fbcb92beeed1990f82f9529535f8767f1e8e8f05
ali27-dev <khanali3229@gmail.com>
2025-11-10 10:01:40 +0500

    First of all i'm added components in one file beacuse i'm beginner, And also i'm used props for render multiple components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,118 @@
+import React from "react";
+import "./index.css";
 function App() {
   return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+export default App;
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>OUR MENU</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+        voluptatibus?
+      </p>
+      <Pizza
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and some other things"
+        photoName="pizzas/margherita.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={12}
+      />
+      {/* <Pizza /> */}
+    </main>
+  );
+}
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <p>{props.price + 3}</p>
+      </div>
+    </div>
+  );
+}
+function Footer() {
+  const houre = new Date().getHours();
+  const openTime = 11;
+  const closeTime = 22;
+  const condition = houre >= openTime && houre <= closeTime;
+  console.log(condition);
+  // if (houre >= openTime && houre <= closeTime) alert("We are currentlty open");
+  // else alert("We are currently close");
+  return (
     <>
-      <h1>Hello World</h1>
+      <p>
+        {new Date().toLocaleTimeString()} We're open until 22:00 . Come visit us
+        or order online.
+      </p>
     </>
   );
 }
-
-export default App;
